b9b6f4b11e87c1fd48c967ba44f743b9af2d80cc
fariahossainlaiju <fariahossainladdu12@gmail.com>
2026-07-22 16:12:30 +0600

    Create data_types.py
    Learned Python data types using type() function.

---
+++ data_types.py
@@ -0,0 +1,22 @@
+# Python Data Types
+
+name = "Faria"          # String
+age = 25                # Integer
+height = 5.2            # Float
+is_student = True       # Boolean
+complex_num = 2 + 3j    # Complex
+
+print("Name:", name)
+print("Type:", type(name))
+
+print("Age:", age)
+print("Type:", type(age))
+
+print("Height:", height)
+print("Type:", type(height))
+
+print("Student:", is_student)
+print("Type:", type(is_student))
+
+print("Complex Number:", complex_num)
+print("Type:", type(complex_num))
