24ece2a7ba065175399dc641fe8f4e61fe1076e0
fariahossainlaiju <fariahossainladdu12@gmail.com>
2026-07-23 13:57:09 +0600

    Create variables.py
    Learned Python variables, assignment, and swapping.

---
+++ variables.py
@@ -0,0 +1,58 @@
+#Variables 
+
+name="Faria"
+age=23
+height=5.2
+weight="51kg"
+country="Bangladesh"
+favorite_colour="White" 
+
+
+print("Name:",name)
+print("Age:",age)
+print("height:",height)
+print("weight:",weight)
+print("Country:",country)
+print("favorite_colour:",favorite_colour)
+
+# Changing the value of a variable 
+
+city="Dhaka"
+print("before:",city)
+city="comilla"
+print("after:",city)
+
+
+#Multiple Assignment 
+
+x,y,z=20,40,80
+
+print(x)
+print(y)
+print(z)
+
+
+#Variables with the same value
+
+
+a=b=c=100
+print(a)
+print(b)
+print(c)
+
+
+#Variable Swapping 
+
+
+first=10
+second =50
+
+print("Befor Swapping")
+print("First=",first)
+print("Second=",second)
+
+first,second = second,first
+
+print("After Swapping")
+print("First=",first)
+print("Second=",second )
