bf97ce2331d51950410598c08b6ff686de64b8b4
fariahossainlaiju <fariahossainladdu12@gmail.com>
2026-07-25 17:37:31 +0600

    Create input_output.py
    Learned Python input() and output().

---
+++ input_output.py
@@ -0,0 +1,21 @@
+print("==Student Information==")
+
+name=input("Enter your name: ")
+
+age=int(input("Enter your age: "))
+
+height=float(input("Enter you height:"))
+
+country=input("Enter your country:")
+
+colour=input("Enter your favourite colour:")
+
+
+
+print("== Student Details ==")
+
+print("name",name)
+print("your age is",age)
+print("your height is",height)
+print("your country is",country )
+print("your favourite colour",colour)
