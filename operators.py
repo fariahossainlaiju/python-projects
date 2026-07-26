00094dc02eaa4bc5e3a08b25582a18401859d90e
fariahossainlaiju <fariahossainladdu12@gmail.com>
2026-07-26 17:57:50 +0600

    Add Python Operators project

---
+++ operators.py
@@ -0,0 +1,55 @@
+print("==Arithmetic Operators==")
+
+a=20
+b=5
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Floor division:",a//b)
+print("Modulus:",a%b)
+print("power:",a**b)
+
+
+print("==Comparison Operators==")
+
+print("a==b:",a==b)
+print("a!=b:",a!=b)
+print("a<b:",a<b)
+print("a>b:",a>b)
+print("a<=b:",a<=b)
+print("a>=b:",a>=b)
+
+
+print("==Logical Operators==")
+
+x=True
+y=False
+
+print("x and y:",x and y)
+print("x or y:",x or y)
+print("not x:",not x)
+
+
+print("==Assignment Operators ==")
+
+num =10
+
+print("Original:",num)
+
+num +=5
+
+print("After +=:",num)
+
+num -= 3
+
+print("After -=:",num)
+
+num *=2
+
+print("After*=:",num)
+
+num /= 4
+
+print("After /=",num)
