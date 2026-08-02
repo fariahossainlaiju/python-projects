5b13d159e7022e8815de8b526d42e6f7f5477758
fariahossainlaiju <fariahossainladdu12@gmail.com>
2026-08-02 07:21:31 +0600

    Create if_else.py
    Added my sixth Python project.
    
    This project demonstrates:
    - if statement
    - if-else statement
    - if-elif-else ladder
    - User input with input()
    - Converting input using int()
    - Student result and grade checker
    - Decision making based on marks
    
    This project helped me understand Python conditional statements.

---
+++ if_else.py
@@ -0,0 +1,38 @@
+print("==Student Result checker==")
+
+name=input("enter your name:")
+marks=int(input("enter your marks:"))
+
+print("== Result ==")
+print("student:",name)
+print("marks:",marks)
+
+
+if marks >=80:
+    print("grade:A+")
+    print("Excellent! ")
+    
+    
+elif marks >70:
+    print(" grade:A")
+    print("Very good!")
+    
+    
+elif marks >60:
+    print("grade:A-")
+    print("good!") 
+    
+       
+elif marks >50:
+    print("grade:B") 
+    print("keep improving!")
+    
+                
+elif marks >40:
+    print("grade:C") 
+    print("you passed! ") 
+    
+
+elif :
+    print(" grade:F")  
+    print("sorry, you failed. ")
