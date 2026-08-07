89343aecb212cee4482c12226e4b41b9a723b4dd
fariahossainlaiju <fariahossainladdu12@gmail.com>
2026-08-07 20:38:14 +0600

    Create loops.py
    Added my seventh Python project.
    
    This project demonstrates:
    - For loop
    - While loop
    - range() function
    - Sum calculation using loops
    - Multiplication table
    - User input
    
    This project helped me understand how to repeat tasks efficiently using loops in Python.

---
+++ loops.py
@@ -0,0 +1,40 @@
+print("==Python Loops==")
+
+
+#For Loops
+print("==For Loops==")
+
+for i in range(1,6):
+    print("nambar:",i)
+
+#while loops
+print("==While Loops==")
+ 
+count = 1
+
+
+while count<= 5:
+     print("count:",count)
+     count += 1
+ 
+     
+             
+#Sum using for loops
+print("==Sum of Number==")  
+
+total=0
+      
+for i in range (1,6):
+    total+=i
+    print("Total:",total)
+    
+    
+    
+#Multiplication Table 
+print("==Multiplication Table==")
+
+nambar =int(input("Enter a nambar:"))
+
+for i in range (1,11):
+       print(nambar,"x",i,"=",nambar * i )
+       
